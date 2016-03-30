94ce2a7a74ef15a33ac3f1994696df0b4b9134b2
B.Misra <basabendra.misra@capgemini.com>
2016-03-30 18:03:11 +0530

    memcached changes

--- src/main/java/com/basab/example/SpringBootGradleBasedDemoApplication.java
+++ src/main/java/com/basab/example/SpringBootGradleBasedDemoApplication.java
@@ -5,42 +5,59 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import argo.jdom.JdomParser;
+import argo.jdom.JsonNode;
+import argo.jdom.JsonRootNode;
+import argo.saj.InvalidSyntaxException;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+import net.spy.memcached.auth.AuthDescriptor;
+import net.spy.memcached.auth.PlainCallbackHandler;
+import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.ConnectionFactoryBuilder;
+import net.spy.memcached.AddrUtil;
+
 @SpringBootApplication
 @RestController
 public class SpringBootGradleBasedDemoApplication {
 
 	@RequestMapping("/hellotest")
 	public String method() {
-		
-		return "HELLO TEST from Basab";
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGradleBasedDemoApplication.class, args);
-		
+		 MemcachedClient mc = null;
 		try {
 		    String vcap_services = System.getenv("VCAP_SERVICES");
 		    if (vcap_services != null && vcap_services.length() > 0) {
-		        // parsing memcachedcloud credentials
-		        JsonRootNode root = new JdomParser().parse(vcap_services);
-		        JsonNode "memcachedcloudNode = root.getNode("memcachedcloud");
-		        JsonNode credentials = "memcachedcloudNode.getNode(0).getNode("credentials");
+		        // parsing rediscloud credentials
+		    	JsonRootNode root = new JdomParser().parse(vcap_services);
+		        JsonNode memcachedcloudNode = root.getNode("memcachedcloud");
+		        JsonNode credentials = memcachedcloudNode.getNode(0).getNode("credentials");
 
 		        // building the memcached client
 		        AuthDescriptor ad = new AuthDescriptor(new String[] { "PLAIN" },
 		                new PlainCallbackHandler(credentials.getStringValue("username"), credentials.getStringValue("password")));
 
-		        MemcachedClient mc = new MemcachedClient(
+		        mc = new MemcachedClient(
 		                  new ConnectionFactoryBuilder()
 		                      .setProtocol(ConnectionFactoryBuilder.Protocol.BINARY)
 		                      .setAuthDescriptor(ad).build(),
 		                  AddrUtil.getAddresses(credentials.getStringValue("servers")));
-
 		    }
-		} catch (InvalidSyntaxException ex) {
+		} catch (Exception ex) {
 		    // vcap_services could not be parsed.
-		} catch (IOException ex) {
-		    // the memcached client could not be initialized.
+			ex.printStackTrace();
 		}
+		
+		
+		mc.set("foo", 0, "bar");
+		Object value = mc.get("foo");
+		return "HELLO TEST from Basab...value from memcached:"+value;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleBasedDemoApplication.class, args);
+		
+
 	}
 }
